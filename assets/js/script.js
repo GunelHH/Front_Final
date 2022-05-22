11682854fcd7b9f17ab2afd39c10f9d25d5b64e6
Gunel <gunelfh@code.edu.az>
2022-05-22 20:37:14 +0400

    nav-carousel

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  dots: false,
+  mouseDrag: false,
+  autoplay: true,
+  autoplayHoverPause: true,
+  smartSpeed: 1000,
+  navigation: false,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
